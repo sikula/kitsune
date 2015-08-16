990c61ad54086850ace682d45dee7f86cd9dd745
Peter Sikula <peter@sikula.com>
2015-08-16 19:01:12 +0000

    fixed minor formatting issue

--- lib/kitsune/core/report/formats/csv.rb
+++ lib/kitsune/core/report/formats/csv.rb
@@ -1,16 +1,5 @@
 require 'csv'
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-# The name of the class is equivalent to the name of the file
-# without the extension
->>>>>>> 4996af7203b0f90794bd66a9ea63aad0b12cfed3
-=======
-# The name of the class is equivalent to the name of the file
-# without the extension
->>>>>>> 4996af7203b0f90794bd66a9ea63aad0b12cfed3
 csv = Module.new do
   def self.format(input)
     CSV.generate do |csv|

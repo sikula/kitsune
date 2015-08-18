0ab4a3cfb06008d65246e85385c69ebaabe4431d
Peter Sikula <peter@sikula.com>
2015-08-18 08:17:21 +0000

    minor adjustments to project structure and code cleanup

--- lib/kitsune/report/formats/html.rb
+++ lib/kitsune/report/formats/html.rb
@@ -3,7 +3,7 @@ require 'erubis'
 module HTML
   def self.format(input)
     template =
-      File.expand_path("../assets/templates/reporting/to_html_report.ktp")
+      File.expand_path("../data/templates/reporting/to_html_report.ktp")
 
     Erubis::Eruby.new(
       File.read(template), :pattern => "{% %}").result(:results => input)
